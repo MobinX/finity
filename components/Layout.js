8deed3f4289f346b269efd927fb542704e177101
MobinX <mobin0219@gmail.com>
2022-01-18 11:19:00 +0000

    init

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+export const Layout = ({ childrean }) => {
+  return (
+    <div className="">
+      <Navbar />
+      <Sidebar />
+      {childrean}
+    </div>
+  );
+};
+const Navbar = () => {
+  return <div className=""></div>;
+};
+const Sidebar = () => {
+  return (
+  <div className="">
+      
+  </div>
+  );
+};
